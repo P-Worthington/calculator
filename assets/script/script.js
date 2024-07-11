903b9ba545050d9473eb0bc7f46d9de2d7a19fb8
Paul Worthington <pworthington17@gmail.com>
2024-07-11 15:33:22 +0000

    event handlers added to all buttons

---
+++ assets/script/script.js
@@ -0,0 +1,65 @@
+document.getElementById("open-bracket").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("close-bracket").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("percent").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("ac").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("7").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("8").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("9").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("division").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("4").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("5").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("6").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("multiply").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("1").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("2").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("3").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("minus").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("0").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("point").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("equals").addEventListener("click", function() {
+    addToCalc();
+});
+document.getElementById("plus").addEventListener("click", function() {
+    addToCalc();
+});
+
+
+  function addToCalc() {
+    console.log("Hello")
+  }
