55c9b50bde2effddd4913af399082ed1d08d18a1
Paul Worthington <pworthington17@gmail.com>
2024-07-11 15:46:49 +0000

    numbers add to result line when clicked

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,65 +1,66 @@
 document.getElementById("open-bracket").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("(");
 });
 document.getElementById("close-bracket").addEventListener("click", function() {
-    addToCalc();
+    addToCalc(")");
 });
 document.getElementById("percent").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("%");
 });
 document.getElementById("ac").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("ac");
 });
 document.getElementById("7").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("7");
 });
 document.getElementById("8").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("8");
 });
 document.getElementById("9").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("9");
 });
 document.getElementById("division").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("÷");
 });
 document.getElementById("4").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("4");
 });
 document.getElementById("5").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("5");
 });
 document.getElementById("6").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("6");
 });
 document.getElementById("multiply").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("x");
 });
 document.getElementById("1").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("1");
 });
 document.getElementById("2").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("2");
 });
 document.getElementById("3").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("3");
 });
 document.getElementById("minus").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("-");
 });
 document.getElementById("0").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("0");
 });
 document.getElementById("point").addEventListener("click", function() {
-    addToCalc();
+    addToCalc(".");
 });
 document.getElementById("equals").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("=");
 });
 document.getElementById("plus").addEventListener("click", function() {
-    addToCalc();
+    addToCalc("+");
 });
 
 
-  function addToCalc() {
-    console.log("Hello")
+  function addToCalc(a) {
+    text = document.getElementById("result-text").innerHTML;
+    document.getElementById("result-text").innerHTML = text + a;
   }
